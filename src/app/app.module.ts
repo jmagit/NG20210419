970c81b8795f2cc33221dca7f6e1a917a5cbece3
Javier <jmartin@grupoloyal.com>
2021-04-20 10:26:19 +0200

    Modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { MyCoreModule } from 'src/my-core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CommonServicesModule } from './common-services';
+import { MainModule } from './main';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule, FormsModule,
+    MainModule, CommonServicesModule, MyCoreModule,
     AppRoutingModule
   ],
   providers: [],
