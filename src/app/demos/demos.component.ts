26435f2bf4dda55ebb03dde31e57dc25f35c41d3
Javier <jmartin@grupoloyal.com>
2021-04-21 10:42:49 +0200

    Clase del componente

--- src/app/demos/demos.component.ts
+++ src/app/demos/demos.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Unsubscribable } from 'rxjs';
+import { LoggerService } from 'src/my-core';
 import { NotificationService, NotificationType } from '../common-services';
 
 @Component({
@@ -9,8 +10,53 @@ import { NotificationService, NotificationType } from '../common-services';
 })
 export class DemosComponent implements OnInit, OnDestroy {
   private suscriptor: Unsubscribable;
+  private nombre = 'mundo';
+  public listado = [
+    { id: 1, nombre: 'Madrid'},
+    { id: 2, nombre: 'barcelona'},
+    { id: 3, nombre: 'VALENCIA'},
+    { id: 4, nombre: 'a Coruña'},
+  ];
+  idProvincia = 2;
 
-  constructor(public vm: NotificationService) { }
+  resultado: string = null;
+  visible = true;
+  estetica = { importante: true, error: false, urgente: true };
+  fontSize = 24;
+
+  constructor(public vm: NotificationService, private out: LoggerService) { }
+
+  get Nombre(): string { return this.nombre; }
+  set Nombre(valor: string) {
+    if(valor === this.nombre) return;
+    // validacion
+    this.nombre = valor;
+    this.out.log('Ha cambiado el nombre');
+  }
+
+  saluda() {
+    this.resultado = `Hola ${this.Nombre}`;
+  }
+  despide() {
+    this.resultado = `Adios ${this.Nombre}`;
+  }
+  di(algo: string) {
+    this.resultado = `Dice ${algo}`;
+  }
+
+  alterna() {
+    this.visible = !this.visible;
+    this.estetica.importante = !this.estetica.importante;
+    this.estetica.error = !this.estetica.error;
+  }
+
+  calcula(a: number, b: number): number { return a + b; }
+
+  add(provincia: string) {
+    const id = this.listado.length === 0 ? 1 : (this.listado[this.listado.length].id + 1);
+    this.listado.push({ id, nombre: provincia });
+    this.idProvincia = id;
+  }
 
   ngOnInit(): void {
     this.suscriptor = this.vm.Notificacion.subscribe(n => {
